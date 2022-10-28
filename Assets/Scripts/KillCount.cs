c9124f61328f999621011ff1b6f0be5c8b3f4701
Mathias <mathnagah@gmail.com>
2022-10-28 12:05:04 -0500

    Script KillCount, prefab player

--- Assets/Scripts/KillCount.cs
+++ Assets/Scripts/KillCount.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class KillCount : MonoBehaviour
 {
 	public int kills;
-	public Text textKills;
+	public TMP_Text textKills;
 
 	private void Update()
 	{
